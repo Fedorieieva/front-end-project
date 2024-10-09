089ac174c4bec102caf997d649d4334674dbd60c
Nazar <ilchishin.nazar@gmail.com>
2024-10-09 14:17:32 +0300

    Fixed ellipsis bug

--- src/components/ProjectCardWrapper/ProjectCardWrapper.jsx
+++ src/components/ProjectCardWrapper/ProjectCardWrapper.jsx
@@ -1,13 +1,13 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Container from "@/components/Container/Container.jsx";
 import ProjectCard from "@/components/ProjectCard/ProjectCard.jsx";
 import Plus from './icons/plus.svg?react';
 
-import './ProjectCardWrapper.scss'
+import './ProjectCardWrapper.scss';
 import Button from "@/components/Button/Button.jsx";
 import ProjectModal from "@/components/ProjectModal/ProjectModal.jsx";
 
-const ProjectCardWrapper = ({data}) => {
+const ProjectCardWrapper = ({ data }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
@@ -19,25 +19,23 @@ const ProjectCardWrapper = ({data}) => {
                             className='add-project-btn'
                             onClick={() => setIsModalOpen(true)}
                         >
-                            <Plus/>
+                            <Plus />
                         </Button>
                     </li>
 
-                    {
-                        data.map((item) => (
-                            <li className='project-card__item' key={item.id}>
-                                <ProjectCard project={item}/>
-                            </li>
-                        ))
-                    }
+                    {data.map((item) => (
+                        <li className='project-card__item' key={item.id}>
+                            <ProjectCard project={item} />
+                        </li>
+                    ))}
                 </ul>
             </div>
 
             {isModalOpen && (
-                <ProjectModal onClose={() => setIsModalOpen(false)}/>
+                <ProjectModal onClose={() => setIsModalOpen(false)} />
             )}
         </Container>
-    )
-}
+    );
+};
 
-export default ProjectCardWrapper
+export default ProjectCardWrapper;
